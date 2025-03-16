cfb5052f4f9a31d019c12bba1121cfb99a143ba5
nathanwiddi <nathanwiddi@gmail.com>
2025-03-16 19:57:18 -0400

    initial commit of go pokedex

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ithub.com/nathanwiddi/pokedexcli
+
+go 1.24.0
---
+++ repl_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "hello there world  ",
+			expected: []string{"hello", "there", "world"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// Check the length of the actual slice against the expected slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		if len(actual) != len(c.expected) {
+			t.Errorf("actual length %v is not the same as expected length %v", len(actual), len(c.expected))
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+			if word != expectedWord {
+				t.Errorf("word %v doesn't match expectedWord %v", word, expectedWord)
+			}
+
+		}
+	}
+}
